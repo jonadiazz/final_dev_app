89cdc9572c18aa0bfd428fd512d1a841dec8ee11
Ubuntu <ubuntu@ip-172-31-26-60.us-west-1.compute.internal>
2015-10-04 16:19:12 +0000

    successfully created page,logo,pulled tables with input of data tested plus layout organization of page

--- dev_app/admin.py
+++ dev_app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import page, logo, pulled
+#,logo,pulled
 
+class logoInline(admin.TabularInline):
+	model=	logo
+	extra = 1
+	
+class pulledInline(admin.StackedInline):
+	model = pulled
+	extra = 0
+
+class pageAdmin(admin.ModelAdmin):
+	fieldsets = [
+	(None,	{'fields': ['page_title','page_domain','page_url']}),
+	]
+	inlines = [logoInline, pulledInline]
+
+admin.site.register(page, pageAdmin)
+#,logo,pulled)
 # Register your models here.
--- dev_app/models.py
+++ dev_app/models.py
@@ -2,4 +2,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class page(models.Model):
+	page_url=	models.CharField(max_length=500)
+	page_title=	models.CharField(max_length=200)
+	page_domain=	models.CharField(max_length=50)
+
+class logo(models.Model):
+	page_domain= models.ForeignKey(page,on_delete=models.CASCADE)
+	logo_url=	models.CharField(max_length=500)
+
+class pulled(models.Model):
+	logo_url=	models.ForeignKey(logo,on_delete=models.CASCADE)
+	page_url=	models.ForeignKey(page,on_delete=models.CASCADE)
+	date_pulled=	models.DateTimeField('date pulled')
+
+
 # Create your models here.
