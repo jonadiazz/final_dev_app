a3b6b2bcf7a876dab012a51b8fb66d28bd7d3885
Ubuntu <ubuntu@ip-172-31-26-60.us-west-1.compute.internal>
2015-10-04 16:42:37 +0000

    layout of Pages done. Referencing tables added to page with integrity constraints successfully

--- dev_app/admin.py
+++ dev_app/admin.py
@@ -1,20 +1,21 @@
 from django.contrib import admin
-from .models import page, logo, pulled
+from .models import page, logo, pull
 #,logo,pulled
 
 class logoInline(admin.TabularInline):
 	model=	logo
 	extra = 1
 	
-class pulledInline(admin.StackedInline):
-	model = pulled
+class pullInline(admin.StackedInline):
+	model = pull
 	extra = 0
 
 class pageAdmin(admin.ModelAdmin):
 	fieldsets = [
 	(None,	{'fields': ['page_title','page_domain','page_url']}),
 	]
-	inlines = [logoInline, pulledInline]
+	inlines =	[logoInline, pullInline]
+	list_display=	('page_title','page_domain',)
 
 admin.site.register(page, pageAdmin)
 #,logo,pulled)
--- dev_app/models.py
+++ dev_app/models.py
@@ -11,8 +11,8 @@ class logo(models.Model):
 	page_domain= models.ForeignKey(page,on_delete=models.CASCADE)
 	logo_url=	models.CharField(max_length=500)
 
-class pulled(models.Model):
-	logo_url=	models.ForeignKey(logo,on_delete=models.CASCADE)
+class pull(models.Model):
+	#logo_url=	models.ForeignKey(logo,on_delete=models.CASCADE)
 	page_url=	models.ForeignKey(page,on_delete=models.CASCADE)
 	date_pulled=	models.DateTimeField('date pulled')
 
